8eeb0f8a5814ce005f90eae7d701775681664c39
ludotenoaa <ludovic.tenorio@noaa.gov>
2022-01-13 10:07:45 -0500

    Import Triton-master

---
+++ README.md
@@ -0,0 +1,22 @@
+# Triton
+
+The Triton Software Package is aimed at evaluating long duration, continuous and scheduled duty-cycle time series. 
+
+The Triton base package was developed by Dr. Sean M. Wiggins at Scripps Institution of Oceanography, UC San Diego.
+
+Remoras are developed by contributors at Scripps Institution of Oceanography and San Diego State University.
+
+For instructions, please check out [our wiki](https://github.com/MarineBioAcousticsRC/Triton/wiki).
+
+If you do not have a Matlab installation, please check out out our [standalone version](https://github.com/MarineBioAcousticsRC/Triton-Compiled).
+
+Non-GitHub Users: You can obtain all of the source code by clicking the green "code" button and selecting "Download ZIP". However we encourage you to clone the repository so that you can easily recieve updates and bug fixes. 
+
+Current and past funders include:  
+- NOAA Protected Species Toolbox Initiative  
+- NAVFAC Living Marine Resources Program  
+
+
+## Batch/MultiDir LTSA Remora development TO DOs
+- add gui/step to check output filenames and change if wanted
+- test Remora folder alone with flac version
---
+++ wavname2dnum.m
@@ -0,0 +1,74 @@
+function [dnums] = wavname2dnum(filenames, dispOn)
+% Parses .wav file names and converts them to matlab datenums
+% Works on single or multiple file names
+% Supports 5 filename formats:
+%   1.  yymmdd-HHMMSS
+%   2.  yymmdd_HHMMSS
+%   3.  yyyymmdd_HHMMSS 
+%   4.  yymmddHHMMSS
+%   5.  yyyymmddTHHMMSS
+
+% This matches both underscores and hyphens, but don't know how to handle
+% the datenum formatting...maybe useful later?
+% regexp(fname,'\d{4}[-_]\d{4}','match','split')
+
+if nargin <2
+    dispOn = 1; % added to limit printing of dnum format during LTSA creation
+end
+
+% start with the default date format
+filenamesc = cellstr(filenames);
+date_strs = regexp(filenamesc,'\d{6}[-]\d{6}','match'); 
+date_fmt = 'yymmdd-HHMMSS';
+
+
+if isempty(date_strs{1})
+    date_fmt = 'yymmddTHHMMSSZ';
+    date_strs = regexp(filenamesc,'\d{6}[T]\d{6}[Z]','match' );
+    if ~isempty(date_strs{1}) && dispOn == 1
+        disp('Using ISO8601 date format yymmddTHHMMSSZ in filename');
+    end
+end
+
+if isempty(date_strs{1}) % not a PAMguard file, try avisoft or Soundtrap filename
+    date_fmt = 'yymmddHHMMSS';
+    date_strs = regexp(filenamesc,'\d{12}','match' );
+    if ~isempty(date_strs{1}) && dispOn == 1
+        disp('Using avisoft and SoundTrap filename format yymmddHHMMSS');
+    end
+end
+
+if isempty(date_strs{1}) % not just an underscore problem, try PAMGuard filename
+    date_fmt = 'yyyymmdd_HHMMSS'; % PAMGuard default file format 
+    date_strs = regexp(filenamesc,'\d{8}[_]\d{6}','match');
+    if ~isempty(date_strs{1}) && dispOn == 1
+        disp('Using PAMGuard filename format yyyymmdd_HHMMSS');
+    end
+end
+
+if isempty(date_strs{1}) % using underscores presumably
+    date_fmt = 'yymmdd_HHMMSS';
+    date_strs = regexp(filenamesc,'\d{6}[_]\d{6}','match');
+end
+
+if isempty(date_strs{1}) % try AMAR filename - e.g., AMAR613.20190604T182000Z.wav
+    date_fmt = 'yyyymmddTHHMMSS';
+    date_strs = regexp(filenamesc,'\d{8}[T]\d{6}','match');
+    if ~isempty(date_strs{1})
+        disp('Using AMAR filename format yyyymmddTHHMMSS');
+    end
+end
+
+if isempty(date_strs{1})
+    disp('Unknown filename date format.  Please use one of the following:');
+    disp('*yymmdd-HHMMSS*.wav');
+    disp('*yymmdd_HHMMSS*.wav');
+    disp('*yyyymmdd_HHMMSS*.wav');
+    disp('*yymmddHHMMSS*.wav'); 
+    date_fmt = 'yymmdd-HHMMSS';
+    dnums = [];
+	return
+end 
+
+dnums = cellfun(@(x)datenum(x,date_fmt),date_strs,'UniformOutput',false);
+dnums = cell2mat(dnums)'; % output of cellfun is a cell
