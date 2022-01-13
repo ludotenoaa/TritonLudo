4e4b19ee1ae58c37234059c9c82f68c2b67168f5
ludotenoaa <ludovic.tenorio@noaa.gov>
2022-01-13 10:27:19 -0500

    Update write_ltsahead.m
    fixed bug
    switched if isempty to if ~isfield

--- write_ltsahead.m
+++ write_ltsahead.m
@@ -15,7 +15,7 @@ global PARAMS
 % if it has already been specified, don't have to do anything (this happens
 % when using mk_ltsa_multidir.m)
 % if making single LTSA through triton gui, prompt for filename
-if isempty(PARAMS.ltsa.outfile)
+if ~isfield(PARAMS.ltsa,'outfile')
     filterSpec1 = '*.ltsa';
     boxTitle1 = 'Save LTSA File';
     % user interface retrieve file to open through a dialog box
